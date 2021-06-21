22488fdc705f98442eb49c95f2c30c79ef010363
Rodolfo Londero <rodolfopl@gmail.com>
2021-06-21 15:33:45 -0300

    App to read trn file em get the time values

---
+++ get-time-simulation/main.py
@@ -0,0 +1,39 @@
+import os
+import time
+import pandas as pd
+import plotly.express as px
+import re
+from pprint import pprint
+
+def get_time_from_file(path):
+    filename, extension = os.path.splitext(path)
+    
+    pattern_seconds = re.compile("t_sec:\s([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?\ss")
+    pattern_minutes = re.compile("t_min:\s([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?\smin")
+    pattern_hour    = re.compile("t_h:\s+([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?\sh")
+    
+    t_sec = list()
+    t_min = list()
+    t_h   = list()
+
+    for i, line in enumerate(open(path)):
+        check_pattern(pattern_seconds, line, t_sec)
+        check_pattern(pattern_minutes, line, t_min)
+        check_pattern(pattern_hour, line, t_h)
+
+    # print(f"Total seconds: {sum(t_sec)}")
+    # print(f"Total minutes: {sum(t_min)}")
+    # print(f"Total hours: {sum(t_h)}")
+    
+    print("Fim")
+def check_pattern(pattern: str, line: str, lista: list):
+    for match in re.finditer(pattern, line):
+        lista.append(float(match.group().strip().split()[-2]))
+
+
+if __name__ == '__main__':
+
+    diretorio = os.path.dirname(__file__)
+    path = os.path.join(diretorio, "_HOA_4.16kV_Malha_01_300_pontos_init.trn")
+
+    get_time_from_file(path=path)
