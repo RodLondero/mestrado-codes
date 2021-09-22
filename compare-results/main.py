89eac185e5c454e145b0a09298f10e2575629e83
Rodolfo Londero <rodolfopl@gmail.com>
2021-09-22 20:33:50 -0300

    Formatting

--- compare-results/main.py
+++ compare-results/main.py
@@ -5,6 +5,6 @@ from app2.app import Ui
 
 if __name__ == "__main__":
 
-    app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
-    window = Ui(os.getcwd()) # Create an instance of our class
-    app.exec_() # Start the application
+    app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
+    window = Ui(os.getcwd())                # Create an instance of our class
+    app.exec_()                             # Start the application
